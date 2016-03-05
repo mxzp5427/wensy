22fda06ca9f59fc9fea342f0196d800c9ab87ec1
mxzp5427 <mxzp5427@163.com>
2016-03-06 00:02:48 +0800

    资源文件整理

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -30,7 +30,7 @@ return array(
 
     /* URL配置 */
     'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
-    'URL_MODEL'            => 2, //URL模式
+    'URL_MODEL'            => 3, //URL模式
     'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
     'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
 
@@ -42,7 +42,7 @@ return array(
     'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'wensy', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '',  // 密码
+    'DB_PWD'    => 'admin',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'wensy_', // 数据库表前缀
 
--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'T]Y6bf-tMR?}m8S2#yv3x%IZz0k!uoj4=rUKF)*g'); //加密KEY
-define('UC_DB_DSN', 'mysqli://root:@127.0.0.1:3306/wensy'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysqli://root:admin@127.0.0.1:3306/wensy'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'wensy_'); // 数据表前缀，使用Model方式调用API必须配置此项
